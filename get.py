9b1b26845fb841e4a0eb6cd0be02ee95bb9b86bc
test <test@test.com>
2020-02-13 14:01:43 +0330

    seperate get function in two . one for current data and second for forecast and midifir function return.

--- get.py
+++ get.py
@@ -1,11 +1,56 @@
 import requests, cityid
 
 API_KEY = 'f9f5bd14bbc6b24dc1620b7c1d58b826'
-API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
-def get_data(city_name,unitformat='metric',language='en'):
+
+def get_current_data(city_name,unitformat='metric',language='en'):
+    '''
+    this function take city name , unitformat(imperial,metric)
+    and data language and return dictionary useful data of current weather data  
+    '''
+    API_URL = 'http://api.openweathermap.org/data/2.5/weather'
     city_id = cityid.get_city_ID(city_name)
     payload = {'id':city_id,'units':unitformat,'lang':language,"APPID":API_KEY}
-    weather = requests.get(API_URL,params=payload)
-    return weather.json()
+    weather = requests.get(API_URL,params=payload).json()
+    useful_data = {
+            'temperature':weather['main']['temp'] ,
+            'temperature_feel':weather['main']['feels_like'] ,  
+            'weather_condition':weather['weather'][0]['description'],
+            'humidity':weather['main']['humidity'],
+            'wind':weather['wind']['speed'],
+            'clouds':(weather['clouds']['all']),
+            'pressure':weather['main']['pressure'],
+            'sunrise':weather['sys']['sunrise'],
+            'sunset':weather['sys']['sunset'],
+            }
+   
+    return useful_data
+
+
+def get_forecast_data(city_name,unitformat='metric',language='en'):
+    '''
+    this function take city name , unitformat(imperial,metric)
+    and data language and return list of tuple that first element of tuple
+    is time and second element of tuple is dictionary that has weather forecast data
+    for 5 days with data every 3 hours 
+    '''
+    API_URL = 'http://api.openweathermap.org/data/2.5/forecast'
+    city_id = cityid.get_city_ID(city_name)
+    payload = {'id':city_id,'units':unitformat,'lang':language,"APPID":API_KEY}
+    weather = requests.get(API_URL,params=payload).json()
+    
+    
+    useful_data = [(item['dt'],{'temprature':item['main']['temp'],
+                                'temprature_feel':item['main']['feels_like'],  
+                                'pressure':item['main']['pressure'],
+                                'weather_condition':item['weather'][0]['description'],
+                                'clouds':item['clouds']['all'],
+                                'wind':item['wind']['speed'],
+                                }
+                    )
+                   for item in weather['list'] ]
+    
+    
+    
+    return useful_data
 
